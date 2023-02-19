50e999c7f3d84b0e029e869c2527326848ce8520
Inna <shvartz_i@mail.ru>
2023-02-19 07:44:19 +0300

    first try database

--- src/Services/PetRepository.java
+++ src/Services/PetRepository.java
@@ -2,31 +2,39 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import Model.Pet;
 
-public class PetRepository implements IRepository <Pet> {
+public class PetRepository implements IRepository<Pet> {
 
-    private List <Pet> farm;
+    private Statement sqlSt;
+    private ResultSet resultSet;
+    private List<Pet> farm;
 
-           
-    public PetRepository () {
+    public PetRepository() {
         farm = new ArrayList<Pet>();
     };
-        
 
     @Override
-    public List <Pet> GetAll() {
+    public List<Pet> GetAll() {
         return farm;
     }
 
     @Override
     public Pet GetById(int id) {
-        return farm.get(id-1);
+        return farm.get(id - 1);
     }
 
     @Override
-    public void Create (Pet pet) {
+    public void Create(Pet pet) {
         farm.add(pet);
     }
 
@@ -35,7 +43,42 @@ public class PetRepository implements IRepository <Pet> {
     }
 
     @Override
-    public void Remove (int id) {
-        farm.remove(id-1);
+    public void Remove(int id) {
+        farm.remove(id - 1);
+    }
+
+    public static Connection getConnection() throws SQLException, IOException {
+
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream("src/Resources/database.properties")) {
+
+            props.load(fis);
+            String url = props.getProperty("url");
+            String username = props.getProperty("username");
+            String password = props.getProperty("password");
+
+            return DriverManager.getConnection(url, username, password);
+        }
+
+    }
+
+    void example() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = getConnection()) {
+                sqlSt = dbConnection.createStatement();
+                resultSet = sqlSt.executeQuery("SELECT Name, Birthday FROM animals.cats");
+                while (resultSet.next()) {
+
+                    String name = resultSet.getString(1);
+                    LocalDate birthday = resultSet.getDate(2).toLocalDate();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+                    System.out.printf("%s  %s\n", name, formatter.format(birthday));
+                }
+            }
+        } catch (ClassNotFoundException | IOException | SQLException ex) {
+            Logger.getLogger(PetRepository.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
+        } 
     }
 }
